28ba97f431ff790cc8e2ffcd99e799da026b5a36
parmar003 <43446566+parmar003@users.noreply.github.com>
2018-12-14 09:34:59 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>First Page</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+  <h1>Welcome to PHP !!</span></h1>
+
+
+<div class="container">
+
+<nav class="navbar tab col-md-12">
+  <a href="index.php"><h3 class="first"><span>First Page</span></h3></a>
+  <a href="second.php"><h3 class="second"><span>Second Page</span></h3></a>
+  <a href="third.php"><h3 class="third"><span>Third Page</span></h3></a>
+</nav>
+
+<div class="box">
+  <div class="circle"><h3>1</h3></div>
+</div>
+
+</div>
+</body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</html>
---
+++ second.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Second Page</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+  <h1><a>This is Second Page!!</a></h1>
+
+<div class="container">
+
+<div class="tab">
+  <a href="index.php"><h3>First Page</h3></a>
+  <a href="second.php"><h3>Second Page</h3></a>
+  <a href="third.php"><h3>Third Page</h3></a>
+
+<div class="box">
+  <div class="circle"><h3>1</h3></div>
+  <div class="circle2"><h3>2</h3></div>
+</div>
+</div>
+</body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</html>
